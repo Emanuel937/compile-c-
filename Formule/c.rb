f55ef3a994d3b5b94eee82e11789413da99bad5d
emanuel947 <emanuelabizimi@gmail.com>
2024-07-27 20:55:39 +0200

    add information on REAME

--- Formule/c.rb
+++ Formule/c.rb
@@ -1,6 +1,6 @@
-class Compile_c < Formula
+class C < Formula
   desc "This command allow to run the c script withoud query about to compile, just run : c file_name.c"
-  homepage "https://github.com/YOUR_USERNAME/homebrew-my_script"
+  homepage "https://github.com/Emanuel937/homebrew-my_script/compile-c-.git"
   url "https://github.com/YOUR_USERNAME/homebrew-my_script/archive/refs/tags/v1.0.tar.gz"
   sha256 "YOUR_SHA256_CHECKSUM"
   license "MIT"
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# compile-c-
+# HAW IS COMPILE_C ?
+This is a just simple outil to allow to compile C script, typing c "filename.c"
+
+# HOW TO INSTALL IT USING BREW ?
+run brew install c
+
+# EXAMPLE CASE 
+c filename.c
+
+
+# NOTE
+
+After the C is compiled and executed the binary file will be remove.
